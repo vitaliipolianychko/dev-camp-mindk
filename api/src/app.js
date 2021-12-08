1243c1c8fe1fde463a23392c61b7ca2cf38605b1
vitaliipolianychko <vet.polyanychko@gmail.com>
2021-12-08 14:56:08 +0200

    add init api

---
+++ api/src/app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+});
