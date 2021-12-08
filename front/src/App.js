68d68e799e8f59a8a5d4238765de097462886bf3
vitaliipolianychko <vet.polyanychko@gmail.com>
2021-12-08 20:44:54 +0200

    add files

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+
+// components
+import HomePageContainer from './containers/HomePage'
+
+function App() {
+  return (
+    <HomePageContainer/>
+  );
+}
+
+export default App;
